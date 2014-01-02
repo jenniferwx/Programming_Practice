7431ffb79680c4ed36eb4f2d9fe95d856c7f7bc2
jenniferwx <wxjennifer@gmail.com>
2014-01-02 18:10:24 -0500

    Update AnagramString.py

--- AnagramString.py
+++ AnagramString.py
@@ -1,9 +1,10 @@
-/*
+'''
 Given two strings, check if they’re anagrams or not. Two strings are anagrams if they are written using 
 the same exact letters, ignoring space, punctuation and capitalization. Each letter should have the same 
 count in both strings. For example, ‘Eleven plus two’ and ‘Twelve plus one’ are meaningful anagrams of 
 each other.
-*/
+'''
+
 import collections
 
 def getLetters(str0):
