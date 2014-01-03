3da5f70cc61603f65dadfb13ca9714b3c0979fad
jenniferwx <wxjennifer@gmail.com>
2014-01-02 23:15:47 -0500

    Update FindEvenOccurringElement.py

--- FindEvenOccurringElement.py
+++ FindEvenOccurringElement.py
@@ -1,1 +1,15 @@
-def getEven1(arr): counts=collections.defaultdict(int) for num in arr: counts[num]+=1 for num, count in counts.items(): if count%2==0: return num
+'''
+Given an integer array, one element occurs even number of times and all others have odd occurrences. 
+Find the element with even occurrences.
+'''
+def getEven1(arr): 
+  counts=collections.defaultdict(int) 
+  for num in arr: 
+    counts[num]+=1 
+    for num, count in counts.items(): 
+      if count%2==0: 
+        return num
+
+# second solution: XOR (assume there is only one even number)
+def getEven2(arr): 
+  return reduce(lambda x, y: x^y, arr+list(set(arr)))  
