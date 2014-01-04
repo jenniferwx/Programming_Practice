d6742dead24bf551ccf3beba5e1ab49f68651bc4
jenniferwx <wxjennifer@gmail.com>
2014-01-03 19:11:05 -0500

    Update LargestContinuousSum.py

--- LargestContinuousSum.py
+++ LargestContinuousSum.py
@@ -4,8 +4,7 @@ Solutions:
 we start summing up the numbers and store in a current sum variable. 
 when the currentSum becomes negative, we restart the current sum value.
 note: we don't reset the current sum value to zeros (which is the solution for array containing positive numbers), 
-because when the arrary contains all negatives, 
-the result will be the largest negative number.
+because when the arrary contains all negatives, the result will be the largest negative number.
 '''
 
 def largestContinuousSum(arr): 
