6d73d62a8701311440f5ea250a3427e9e12c8bf5
jenniferwx <wxjennifer@gmail.com>
2014-01-05 12:31:23 -0500

    Update WordLadder.py

--- WordLadder.py
+++ WordLadder.py
@@ -4,3 +4,57 @@ changing/adding/removing 1 character at a time, while all intermediate words bei
 Return the transformation chain which has the smallest number of intermediate words.
 
 '''
+
+import collections
+from collections import deque
+
+# construct a word-transform graph
+def constructGraph(dictionary): 
+  graph=collections.defaultdict(list) 
+  letters=string.lowercase  ##
+  for word in dictionary: 
+    for i in range(len(word)): 
+      #remove 1 character 
+      remove=word[:i]+word[i+1:] 
+      if remove in dictionary: 
+        graph[word].append(remove) 
+      
+      #change 1 character 
+      for char in letters: 
+        change=word[:i]+char+word[i+1:] 
+        if change in dictionary and change!=word: 
+          graph[word].append(change) 
+    
+    #add 1 character 
+    for i in range(len(word)+1): 
+      for char in letters: 
+        add=word[:i]+char+word[i:] 
+        if add in dictionary: 
+          graph[word].append(add)   
+          
+  return graph
+
+# perform breath-first search
+def transformWord(graph, start, goal): 
+  paths=collections.deque([ [start] ]) 
+  extended=set() 
+  
+  #Breadth First Search 
+  while len(paths)!=0: 
+    currentPath=paths.popleft() 
+    currentWord=currentPath[-1] # the last word
+    if currentWord==goal: 
+      return currentPath 
+    elif currentWord in extended: 
+      #already extended this word 
+      continue   
+    extended.add(currentWord) 
+    
+    transforms=graph[currentWord] 
+    for word in transforms: 
+      if word not in currentPath: 
+        #avoid loops 
+        paths.append(currentPath[:]+[word])   
+        
+  #no transformation 
+  return []
