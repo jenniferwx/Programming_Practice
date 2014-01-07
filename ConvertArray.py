2920806164e160b64d49345a7c80480fa89fbd84
jenniferwx <wxjennifer@gmail.com>
2014-01-07 16:40:17 -0500

    Create ConvertArray.py

---
+++ ConvertArray.py
@@ -0,0 +1,11 @@
+'''
+Given an array:
+
+[a_1, a_2, ..., a_N, b_1, b_2, ..., b_N, c_1, c_2, ..., c_N ]
+
+convert it to:
+
+[a_1, b_1, c_1, a_2, b_2, c_2, ..., a_N, b_N, c_N]
+
+in-place using constant extra space.
+'''
