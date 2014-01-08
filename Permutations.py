dd9d1d980bfb44abdaa1ae44f39121f919a82cef
jenniferwx <wxjennifer@gmail.com>
2014-01-08 11:09:23 -0500

    Update Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -3,14 +3,14 @@ Generate all permutations of a given string
 '''
 
 def permutations(str):
-    if(len(str)==1)
-    return str[0]
+    if(len(str)==1):
+        return str[0]
     permutation = permutations(str[1:])
     word = str[0]
     result = []
     
     for perm in permutation:
-        for i in range(len(perm)):
+        for i in range(len(perm)+1):  # note: len+1
             result.append(perm[:i]+word+perm[i:])
     
     return result        
