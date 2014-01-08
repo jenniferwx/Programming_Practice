77faecb03a24edd02c61301d817b33d7ee6796d0
jenniferwx <wxjennifer@gmail.com>
2014-01-07 19:05:33 -0500

    Update ConvertArray.py

--- ConvertArray.py
+++ ConvertArray.py
@@ -9,3 +9,23 @@ convert it to:
 
 in-place using constant extra space.
 '''
+
+# Solution 1: use extra space
+
+def getIDX(idx,N):
+   return (idx%3)*N+idx/3
+
+def NewArray(array):
+  N = len(array)/3
+  return [array[getIDX(i,N)] for i in range(len(array))]
+  
+  
+#Solution 2: 
+def convertArray(arr): 
+  N=len(arr)/3 
+  for currentIndex in range(len(arr)): 
+    swapIndex=getIndex(currentIndex, N) 
+    while swapIndex<currentIndex: 
+      swapIndex=getIndex(swapIndex, N) 
+      arr[currentIndex], arr[swapIndex] = arr[swapIndex], arr[currentIndex]
+  
