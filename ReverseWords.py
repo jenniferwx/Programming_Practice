e2f11a4948ec9d2b454003f1544fe006ecbf548e
jenniferwx <wxjennifer@gmail.com>
2014-01-08 13:37:20 -0500

    Update ReverseWords.py

--- ReverseWords.py
+++ ReverseWords.py
@@ -1,9 +1,32 @@
 '''
 Given an input string, reverse all the words. 
-To clarify, input: “Interviews are awesome!” 
+For example:
+input: “Interviews are awesome!” 
 output: “awesome! are Interviews”. 
 
 Consider all consecutive non-whitespace characters as individual words. If there are multiple spaces between words 
 reduce them to a single white space. Also remove all leading and trailing whitespaces. 
 So, the output for ”   CS degree”, “CS    degree”, “CS degree   “, or ”   CS   degree   ” are all the same: “degree CS”
 '''
+
+# push the words to a stack and in the end pop all to reverse 
+def reverseWords(text):
+    words=[] 
+    length=len(text) 
+    space=set(string.whitespace) 
+    index=0 
+    while index<length: 
+      if text[index] not in space: 
+         wordStart=index 
+      while index<length and text[index] not in space: 
+          index+=1 
+      words.append(text[wordStart:index]) 
+      index+=1   
+      print " ".join(reversed(words)) 
+
+
+# other solutions, which might be regarded as cheating ones
+def reverseWords1(text): 
+    print " ".join(reversed(text.split()))   
+def reverseWords2(text): 
+    print " ".join(text.split()[::-1]) 
