89e37d96d3828e986cbe326b9a23b22922661a00
jenniferwx <wxjennifer@gmail.com>
2014-01-12 13:44:07 -0500

    Update FindWordPositionsinText.py

--- FindWordPositionsinText.py
+++ FindWordPositionsinText.py
@@ -2,3 +2,25 @@
 Given a text file and a word, find the positions that the word occurs in the file. We’ll be asked to find 
 the positions of many words in the same file.
 '''
+
+# Solution 1: 
+import collections as col
+import re
+
+def getWords(text): 
+    return re.sub(r'[^a-z0-9]',' ',text.lower()).split() 
+    
+def CreateIndex(text) :
+    index = col.defaultdic(list)
+    words = getWords(text)
+    for pos, word in enumerate(words):
+        index[word].append(pos)
+    return index
+
+def GetIndex(index, word):
+    if word in index:
+       return index[word]
+    else:
+       return []
+      
+        
