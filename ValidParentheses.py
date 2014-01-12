2dd033616cf4fe2f34b8b0a9db60339f8abcd15a
jenniferwx <wxjennifer@gmail.com>
2014-01-11 21:42:36 -0500

    Update ValidParentheses.py

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -2,3 +2,20 @@
 Given a string of opening and closing parentheses, check whether it’s balanced. 
 We have 3 types of parentheses: round brackets: (), square brackets: [], and curly brackets: {}. 
 '''
+
+def isBalanced(expr): 
+    if len(expr)%2!=0: 
+      return False 
+    opening=set('([{') 
+    match=set([ ('(',')'), ('[',']'), ('{','}') ]) 
+    stack=[] 
+    for char in expr: 
+        if char in opening: 
+           stack.append(char) 
+        else: 
+           if len(stack)==0: 
+              return False 
+           lastOpen=stack.pop()
+           if (lastOpen, char) not in match: 
+              return False 
+    return len(stack)==0
