c28bf318369b8dd92385e2963a2fdf9bac18465a
jenniferwx <wxjennifer@gmail.com>
2014-01-11 21:41:00 -0500

    Create ValidParentheses.py

---
+++ ValidParentheses.py
@@ -0,0 +1,4 @@
+'''
+Given a string of opening and closing parentheses, check whether it’s balanced. 
+We have 3 types of parentheses: round brackets: (), square brackets: [], and curly brackets: {}. 
+'''
