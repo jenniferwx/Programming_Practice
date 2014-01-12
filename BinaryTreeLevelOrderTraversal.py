a127c976ca3510318be2fc6e078fde376534c6e8
jenniferwx <wxjennifer@gmail.com>
2014-01-12 11:42:00 -0500

    Update BinaryTreeLevelOrderTraversal.py

--- BinaryTreeLevelOrderTraversal.py
+++ BinaryTreeLevelOrderTraversal.py
@@ -1,3 +1,32 @@
 '''
 '''
 
+class Node:
+    def __init__(self, val = None):
+        self.left, self.right, self.val = None, None, val
+        
+import collections
+
+def LevelOrderPrint(tree):
+    if not tree:
+       return
+    nodes = collections.deque(tree)
+    currentcount, nextcount = 1,0
+    while(len(nodes)!=0):
+        currentnodes = nodes.popleft();
+        currentcount -=1
+        print currentnodes.val
+        if currennodes.left:
+           nodes.append(currentnodes.left)
+           nextcount +=1
+        if currentnodes.right:
+           nodes.append(currentnodes.right)
+           nextcount +=1
+        if currentcount==0:
+           print '\n'
+           currentcount, nextcount = nextcount, currentcount
+        
+    
+    
+      
+
