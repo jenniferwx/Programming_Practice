8749bac279330cdcedd35066f5eb7b9c6e27d970
jenniferwx <wxjennifer@gmail.com>
2014-01-12 18:21:46 -0500

    Update FindWordPositionsinText.py

--- FindWordPositionsinText.py
+++ FindWordPositionsinText.py
@@ -30,3 +30,59 @@ def GetIndex(index, word):
 '''
 it's very useful to use a trie while performing storage and retrieval on text data
 '''
+class Node: 
+      def __init__(self, letter): 
+          self.letter = letter 
+          self.isTerminal = False 
+          self.children = {} 
+          self.positions = []
+
+class Trie: 
+    def __init__(self): 
+        self.root=Node('')   
+        
+    def __contains__(self, word): 
+        current=self.root 
+        for letter in word: 
+            if letter not in current.children: 
+                return False 
+            current=current.children[letter] 
+        return current.isTerminal   
+        
+    def __getitem__(self, word): 
+        current=self.root 
+        for letter in word: 
+            if letter not in current.children: 
+                current.children[letter]=Node(letter) 
+            current=current.children[letter] 
+            current.isTerminal=True 
+        return current.positions   
+        
+    def __str__(self): 
+        self.output([self.root]) 
+        return ''   
+    
+    def output(self, currentPath, indent=''): 
+        #Depth First Search 
+        currentNode=currentPath[-1] 
+        if currentNode.isTerminal: 
+            word=''.join([node.letter for node in currentPath]) 
+            print indent+word+' '+str(currentNode.positions) 
+            indent+=' ' 
+        for letter, node in sorted(currentNode.children.items()): 
+            self.output(currentPath[:]+[node], indent)
+            
+def createIndex2(text): 
+    trie=Trie() 
+    words=getWords(text) 
+    for pos, word in enumerate(words): 
+        trie[word].append(pos) 
+    return trie   
+
+def queryIndex2(index, word): 
+    if word in index: 
+        return index[word] 
+    else: 
+        return []
+        
+        
