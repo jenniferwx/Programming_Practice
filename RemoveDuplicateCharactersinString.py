992810ce598fd089fbefdb0f1f52a6d47929cd2d
jenniferwx <wxjennifer@gmail.com>
2014-01-12 21:45:54 -0500

    Create RemoveDuplicateCharactersinString.py

---
+++ RemoveDuplicateCharactersinString.py
@@ -0,0 +1,13 @@
+'''
+Remove duplicate characters in a given string keeping only the first occurrences. 
+For example, if the input is ‘tree traversal’ the output will be ‘tre avsl’.
+'''
+
+def removeDuplicates(string): 
+    result=[] 
+    seen=set() 
+    for char in string: 
+        if char not in seen: 
+           seen.add(char) 
+           result.append(char) 
+    return ''.join(result)
