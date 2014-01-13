3bddce367871d4cce8de1b2c5077bfa646126092
jenniferwx <wxjennifer@gmail.com>
2014-01-12 21:34:06 -0500

    Create FindNextHigherNumberWithSameDigits.py

---
+++ FindNextHigherNumberWithSameDigits.py
@@ -0,0 +1,4 @@
+'''
+Given a number, find the next higher number using only the digits in the given number. 
+For example if the given number is 1234, next higher number with same digits is 1243
+'''
