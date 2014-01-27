6f0cfed19aed75b5b6aa0bc4f0f685f978f99598
jenniferwx <wxjennifer@gmail.com>
2014-01-27 00:12:39 -0500

    Update SortArrayInASpecificWay.py

--- SortArrayInASpecificWay.py
+++ SortArrayInASpecificWay.py
@@ -15,10 +15,14 @@ def sortarray(array):
             posID = i
             if posID!= negID:
                array[negID], array[posID] = array[posID], array[negID]
+               newID = negID+1
+               while newID!=posID:
+                   array[posID],array[newID] = array[newID], array[posID]   
+                   newID +=1
             negID +=1
     return array
     
 if __name__=='__main__':
-   array = [-1,-5,-11,-3,-2]
+   array = [-5,1,3,4,-2,10,-6]
    array = sortarray(array)
    print array
