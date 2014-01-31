5180d361e5dc1f2c98e6601a2f57cefff3a03f42
jenniferwx <wxjennifer@gmail.com>
2014-01-30 22:48:22 -0500

    Create MaximalProductwhenCuttingRope.java

---
+++ MaximalProductwhenCuttingRope.java
@@ -0,0 +1,8 @@
+/**
+* Given a rope with length n, how to cut the rope into m parts with length n[0], n[1], ..., n[m-1], in order to get 
+* the maximal product of n[0]*n[1]* ... *n[m-1]? We have to cut once at least. 
+* Additionally, the length of the whole length of the rope, as well as the length of each part, are in integer value.
+
+* For example, if the length of the rope is 8, the maximal product of the part lengths is 18. In order to get the
+* maximal product, the rope is cut into three parts with lengths 2, 3, and 3 respectively
+*/
