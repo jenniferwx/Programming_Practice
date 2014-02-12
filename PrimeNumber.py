7a50448c859eb0e941a9b5d0cb49af8bbe306567
jenniferwx <wxjennifer@gmail.com>
2014-02-11 20:32:28 -0500

    Create PrimeNumber.py

---
+++ PrimeNumber.py
@@ -0,0 +1,24 @@
+'''
+Print the top K prime number
+'''
+
+import math
+
+def PrimeNumber(K):
+    i=1
+    num=2
+    while i<=K:
+        r = int(math.sqrt(num))
+        for j in [m+2 for m in range(r)]:
+            if num%j==0:
+             break;
+        if j==r+1:
+           print num
+           i +=1
+        num +=1   
+
+
+if __name__=="__main__":
+    K = 10
+    PrimeNumber(K)
+    
